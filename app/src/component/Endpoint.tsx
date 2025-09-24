5a50c0c18331219180056787f48dad80625e050f
Hawker <kovujacksonbentley@gmail.com>
2025-09-23 21:36:30 -0400

    add endpoint

---
+++ app/src/component/Endpoint.tsx
@@ -0,0 +1,37 @@
+import '../main.css'
+function Endpoint() {
+  return (
+    <div className="flex-vertical border">
+        <p>Endpoint</p>
+        <div className="flex-horizontal">
+            <p>Path:</p>
+            <input type="text" />
+        </div>
+            <div className="flex-horizontal">
+      <label htmlFor="method-input">Method:</label>
+      <input list="methods" name="method-input" />
+      <datalist id="methods">
+        <option value="DELETE" />
+        <option value="GET" />
+        <option value="PATCH" />
+        <option value="POST" />
+        <option value="PUT" />
+      </datalist>
+    </div>
+    <div className="flex-horizontal">
+            <p>Summary:</p>
+            <input type="text" />
+        </div>
+        <div className="flex-horizontal">
+            <p>Description:</p>
+            <input type="text" />
+        </div>
+        <div className="flex-horizontal">
+            <p>Tags:</p>
+            <input type="text" />
+        </div>
+    </div>
+  );
+}
+
+export default Endpoint;
